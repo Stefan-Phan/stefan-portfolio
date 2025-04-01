ee32943dbb2978eba1022fda6399fed599417856
Nguyen Tuan Hung (Stefan) Phan <phany036@mymail.unisa.edu.au>
2025-04-02 00:04:21 +1030

    Finalised everything, make the hamburger icon works

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,35 +1,47 @@
+"use client";
 // components/Header.js
 import Link from "next/link";
+import { useState } from "react";
 
 export default function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <header className="py-6 px-4 flex justify-between items-center">
       <div className="flex items-center">
         <div className="h-6 w-6 bg-red-500 rounded-full mr-3"></div>
         <Link href="/">
           <h1 className="text-black font-bold text-xl md:text-2xl">
-            {" "}
-            {/* Responsive font size */}
             Stefan Phan
             <span className="text-gray-500 font-normal text-base">
-              {" "}
               | Software Engineer
             </span>
           </h1>
         </Link>
       </div>
-      <button className="text-2xl md:hidden">☰</button>{" "}
-      {/* Hamburger menu (hidden on medium+ screens) */}
-      <nav className="hidden md:block">
-        {" "}
-        {/* Hide on mobile, show on medium+ */}
-        <ul className="flex space-x-6 text-sm items-center">
+      <button
+        className={`${isMenuOpen ? "hidden" : "md:hidden"} text-2xl`}
+        onClick={toggleMenu}
+      >
+        ☰
+      </button>
+
+      <nav
+        className={`${
+          isMenuOpen ? "block" : "hidden md:block"
+        } md:flex md:items-center w-full md:w-auto`}
+      >
+        <ul className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-6 text-sm items-center">
           <li>
             <Link href="/resume" className="text-gray-700 hover:text-black">
               Resume
             </Link>
           </li>
-          <li>
+          <li className="hidden md:block">
             <div className="h-6 border-l border-black mx-3"></div>
           </li>
           <li>
@@ -37,7 +49,7 @@ export default function Header() {
               Projects
             </Link>
           </li>
-          <li>
+          <li className="hidden md:block">
             <div className="h-6 border-l border-black mx-3"></div>
           </li>
           <li>
@@ -45,6 +57,13 @@ export default function Header() {
               Contact
             </Link>
           </li>
+          {isMenuOpen && (
+            <li className="md:hidden">
+              <button onClick={toggleMenu} className="text-xl">
+                x
+              </button>
+            </li>
+          )}
         </ul>
       </nav>
     </header>
